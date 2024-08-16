d44f61777b704d0f02739e2ec2a75a46b9359d22
Alan Luna <alanjosuelunacardona@gmail.com>
2024-08-16 13:21:27 -0600

    yes

--- src/main/java/umg/progra2/Main.java
+++ src/main/java/umg/progra2/Main.java
@@ -1,7 +1,19 @@
 package umg.progra2;
+import carpetabot.tareabot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            tareabot bot = new tareabot();
+            botsApi.registerBot(bot);
+            System.out.println("El Bot esta funcionando...................");
+        }
+        catch (TelegramApiException ex){
+            System.out.println("error"+ex.getMessage());
+        }
     }
 }
