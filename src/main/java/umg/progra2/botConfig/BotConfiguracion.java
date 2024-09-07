fb7f809c71c70a16f630ce302216bb93eed813b8
Alan Luna <alanjosuelunacardona@gmail.com>
2024-09-07 08:16:08 -0600

    funcional

---
+++ src/main/java/umg/progra2/botConfig/BotConfiguracion.java
@@ -0,0 +1,12 @@
+package umg.progra2.botConfig;
+
+public class BotConfiguracion {
+
+    public static String getBotUsername() {
+        return "@ChiltepeBot";
+    }
+
+    public static String getBotToken() {
+        return "77322241803:AAFfiMfbIYkQok2KPRscso95jGlbz_gALhs";
+    }
+}
